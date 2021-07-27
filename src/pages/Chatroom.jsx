763a99efa6cb078987b0ca443d5972e9be57b62d
farzanehHaddadi <farzhadadi@gmail.com>
2021-07-27 23:54:11 +0430

    implementing chat engine before real user info

--- src/pages/Chatroom.jsx
+++ src/pages/Chatroom.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import { useUserContext } from "../contexts/userContext";
 const Chatroom = () => {
+  const { firebaseUser: user } = useUserContext();
+
   return (
     <div>
-      <h1>Chat room</h1>
+      <ChatEngine
+        height="calc(100vh - 100px)"
+        userName="admin"
+        userSecret="admin"
+        projectID="e28658d0-5a47-4e0f-9ced-2db58bab8ee7"
+      />
     </div>
   );
 };
